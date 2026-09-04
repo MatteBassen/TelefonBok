fe1386d97e2dcbbad6434d2900f0cbbe23dc7cee
MatteBassen <mattebassen@icloud.com>
2026-09-04 14:30:21 +0200

    laget liste, ordbøker og appendet

---
+++ telefonbok.py
@@ -0,0 +1,14 @@
+telefonbok = []
+
+person1 = {
+   "navn": "mattebassen",
+   "nummer": 94823312
+}
+
+person2 = {
+    "navn": "sverre",
+    "nummer": 46576760
+}
+
+telefonbok.append(person1, person2)
+
